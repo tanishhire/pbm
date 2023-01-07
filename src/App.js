0c10f151bdf79563c4ade5d3695bb472f7213900
Tanish Hire <83215038+tanishhire@users.noreply.github.com>
2023-01-07 18:33:19 +0530

    routing fixes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <>
         <Navbar />
         <Routes>
-          <Route path="/pbm" element={<Hero />} />
+          <Route path="/" element={<Hero />} />
         </Routes>           
         <Routes>
           <Route path="/services" element={<Services />} />
