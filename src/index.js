cb3059b57d8fa1d406235cf1bf08453d4f365fa3
Tanish Hire <83215038+tanishhire@users.noreply.github.com>
2023-01-07 20:21:24 +0530

    hash

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-// import { HashRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 
@@ -11,11 +11,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-    {/* <HashRouter> */}
+    //<BrowserRouter>
+    <HashRouter basename='/'> 
         <App />
-    {/* </HashRouter> */}
-    </BrowserRouter>
+    </HashRouter>
+    //</BrowserRouter>
     
 );
 
